d547d15f23c784a00af79a80bb65f4d589e45ba0
Tung <toilatung@icloud.com>
2020-12-21 11:45:31 +0700

    a

--- ios/Classes/SwiftFlutterZebraSdkPlugin.swift
+++ ios/Classes/SwiftFlutterZebraSdkPlugin.swift
@@ -11,16 +11,25 @@ public class SwiftFlutterZebraSdkPlugin: NSObject, FlutterPlugin {
   }
 
   public func handle(_ call: FlutterMethodCall, result: @escaping FlutterResult) {
+    switch (call.method) {
+    case "printZPLOverTCPIP":
+        onPrZplDataOverTcp(call, result: result)
+        break
+    default:
+        break
+    }
     result("iOS " + UIDevice.current.systemVersion)
   }
 
-  public func onPrZplDataOverTcp(_ call: FlutterMethodCall, result: @escaping FlutterResult, _ theIpAddress: String?) {
-        
+  public func onPrZplDataOverTcp(_ call: FlutterMethodCall, result: @escaping FlutterResult) {
+    let arguments = call.arguments as! Dictionary<String, AnyObject>
+    let theIpAddress = arguments["ip"] as! String
+    let data = arguments["data"] as! String
     let thePrinterConn = TcpPrinterConnection(address: theIpAddress, andWithPort: 9100)
     let success = thePrinterConn?.open() ?? false
-    let zplData = "^XA^FO20,20^A0N,25,25^FDThis is a ZPL test.^FS^XZ"
+//    let zplData = "^XA^FO20,20^A0N,25,25^FDThis is a ZPL test.^FS^XZ"
 //    var errorData: Error? = nil
-    let rep = thePrinterConn?.write(zplData.data(using: .utf8), error: nil)
+    let rep = thePrinterConn?.write(data.data(using: .utf8), error: nil)
     result("rep"+rep!.description+success.description)
     thePrinterConn?.close()
   }
--- lib/flutter_zebra_sdk.dart
+++ lib/flutter_zebra_sdk.dart
@@ -6,12 +6,8 @@ class ZebraSdk {
   static const MethodChannel _channel =
       const MethodChannel('flutter_zebra_sdk');
 
-  static Future<String> get platformVersion async {
-    final String version = await _channel.invokeMethod('getPlatformVersion');
-    return version;
-  }
-
-  static Future<String> printZPLOverTCPIP(String ipAddress, {int port, String data}) async {
+  static Future<String> printZPLOverTCPIP(String ipAddress,
+      {int port, String data}) async {
     final Map<String, dynamic> params = {"ip": ipAddress};
     if (port != null) {
       params['port'] = port;
@@ -22,12 +18,12 @@ class ZebraSdk {
     return await _channel.invokeMethod('printZPLOverTCPIP', params);
   }
 
-  static Future<String> printZPLOverBluetooth(String macAddress, {String data}) async {
+  static Future<String> printZPLOverBluetooth(String macAddress,
+      {String data}) async {
     final Map<String, dynamic> params = {"mac": macAddress};
     if (data != null) {
       params['data'] = data;
     }
     return await _channel.invokeMethod('printZPLOverBluetooth', params);
   }
-
 }
